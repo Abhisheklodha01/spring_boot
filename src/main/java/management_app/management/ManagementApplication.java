0f122c1cec8cfc808ace0a6a4ac8a5feb5e2722d
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-13 22:05:21 +0530

    started spring boot project

---
+++ src/main/java/management_app/management/ManagementApplication.java
@@ -0,0 +1,14 @@
+package management_app.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		SpringApplication.run(ManagementApplication.class, args);
+	}
+
+}
